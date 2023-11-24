e9a3f79cb56c32f3de59bffe85747e44ee34068b
Emile Corvini <71837803+Emile02@users.noreply.github.com>
2023-11-24 13:21:12 +0100

    Update MainTest.java

--- test/MainTest.java
+++ test/MainTest.java
@@ -6,7 +6,8 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class MainTest {
-
+    
+    // simple test to see if the array of keyword is well sorted
     @Test
     public void testKeywordSorting() {
         String[] unsortedKeywords = new String[]{"Pandora", "Pinterest", "Paypal", "Pg&e", "Project free tv", "Priceline", "Press democrat", "Progressive", "Project runway", "Proactive", "Programming", "Progeria", "Progesterone", "Progenex", "Procurable", "Processor", "Proud", "Print", "Prank", "Bowl", "Owl", "River", "Phone", "Kayak", "Stamps", "Reprobe"};
